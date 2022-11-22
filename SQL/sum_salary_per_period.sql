23b6fda15ea6de15a906bccc2bb0df7f51c8414a
Wibi <wppsuhendra@gmail.com>
2022-11-22 23:39:10 +0700

    add comment for sum_salary_per_period.sql

--- SQL/sum_salary_per_period.sql
+++ SQL/sum_salary_per_period.sql
@@ -1,18 +1,18 @@
 truncate table mekari.salary_period;
 
 insert into mekari.salary_period as
-with time_range  as (
+with time_range  as ( --get the list of period (yyyy-mm) from timesheets
 	select distinct extract(year from to_date(date,'yyyy-mm-dd')) as year,extract(month from to_date(date,'yyyy-mm-dd')) as month  from timesheets
 	order by year, month
-), employee_  as (
+), employee_  as ( --get employee data cleansed (resign date and deduplication)
 	SELECT employe_id, branch_id, salary, join_date, case when resign_date='' then '2900-01-01' else resign_date end as resign_date
 	FROM (
 		select *, row_number() over(partition by employe_id order by salary desc) rnum from 
 		employees 
 	) a where rnum = 1
-), time_salary as (
+), time_salary as ( --join the time range with the employee considering join date and resign date
 	select * from time_range a left join employee_ b on to_date(b.join_date,'yyyy-mm-dd')<=to_date(concat(a.year,lpad(a.month::varchar,2,'0'),'28'),'yyyymmdd')
 	and to_date(b.resign_date,'yyyy-mm-dd')>to_date(concat(a.year,lpad(a.month::varchar,2,'0'),'01'),'yyyymmdd')
-), salary_period as (
+), salary_period as ( --summarizing the salary per period
 	select year::int, month::int, branch_id, sum(salary) from time_salary group by 1,2,3
 )select * from salary_period bs order by year, month, branch_id; 
